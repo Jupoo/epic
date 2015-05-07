2e84074bdd0b8faa56d1cefa3a8ec8332aaaf964
jupoo <polintseva.y@party-masters.ru>
2015-05-07 21:19:48 +0300

    epic ui kit

---
+++ js/scripts.js
@@ -0,0 +1,74 @@
+$(document).ready( function() {
+    //Закрытие блока
+    $(document).on("click", ".closing__item", function () {
+        $(this).remove();
+    });
+
+    //radio
+    $(document).on("click", ".pseudo-radio__item input[type='radio']", function () {
+        var radio_wrap=$(this).closest(".pseudo-radio");
+        radio_wrap.find(".pseudo-radio__item").removeClass("pseudo-radio--active");
+        $(this).closest(".pseudo-radio__item").addClass("pseudo-radio--active");
+    });
+
+    //checkbox
+    $(document).on("click", ".pseudo-checkbox input[type='checkbox']", function () {
+        var checkbox_wrap=$(this).closest(".pseudo-checkbox");
+
+        if($(this).prop("checked")==true) {
+            checkbox_wrap.addClass("pseudo-checkbox--active");
+        }
+        else {
+            checkbox_wrap.removeClass("pseudo-checkbox--active");
+        }
+    });
+
+    //tabs
+    $(document).on("click", ".tabs__list-link", function (e) {
+        e.preventDefault();//отмена действий по умолчанию
+        var tab_id=$(this).attr("href");
+        $(".tabs__list").removeClass("tabs--list-active");
+        $(this).closest(".tabs__list").addClass("tabs--list-active");
+        $(".tabs__text-item").removeClass("tabs--text-active");
+        $(tab_id).addClass("tabs--text-active");
+    });
+
+    //Слайдер
+    if($(".bxslider").length>0) {
+        var slider=$(".bxslider").bxSlider({
+            mode: "horizontal",
+            minSlides: 1,
+            maxSlides: 1,
+            moveSlides: 1,
+            //slideWidth: 552,
+            auto: false,
+            autoHover: false,
+            speed: 500,
+            pause: 700,
+            pager: true,
+            controls: true,
+            touchEnabled: true
+        });
+        // установим обработчик события resize
+        $(window).resize(function(){
+            if(slider)
+                slider.reloadSlider();
+        });
+    }
+
+    //Ползунок
+    $(".slider-range__body").slider({
+        range: true,
+        min: 0,
+        max: 15000,
+        values: [1200, 9500],
+        create: function (event, ui) {
+            $(this).find(".ui-slider-handle:eq(0)").html("<span>"+1200+"Р</span>");
+            $(this).find(".ui-slider-handle:eq(1)").html("<span>"+9500+"Р</span>");
+        },
+        slide: function (event, ui) {
+            $(this).find(".ui-slider-handle:eq(0)").html("<span>"+ui.values[0]+"Р</span>");
+            $(this).find(".ui-slider-handle:eq(1)").html("<span>"+ui.values[1]+"Р </span>");
+        }
+    });
+});
